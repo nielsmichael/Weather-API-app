b6bcdac516e1eb4c5c53fdd30dd7a2778d1e8ee1
xXnielsXx <nielsmichael@comcast.net>
2022-05-01 10:57:15 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// var map = new mapboxgl.Map({
+//   container: "map",
+//   style: "mapbox://styles/mapbox/satellite-streets-v11", //this style sometimes displays poorly but I just love this view
+//   zoom: 10,
+// });
+// from Chris
